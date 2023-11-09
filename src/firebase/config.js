7e404d59de6ac2ff5e40fe80f2531e67184ca908
delfinarawson <drawson@udesa.edu.ar>
2023-11-09 10:45:26 -0300

    firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC275ow-53-3Asmxu6QTiPF-2V1t1iNRiw",
-  authDomain: "prog3final-20679.firebaseapp.com",
-  projectId: "prog3final-20679",
-  storageBucket: "prog3final-20679.appspot.com",
-  messagingSenderId: "59075933964",
-  appId: "1:59075933964:web:00c80d8a4d0791be42fb29"
+  apiKey: "AIzaSyC2Lj9Z-qNsP1Y0EX2U134PORKLYtp5n1Y",
+  authDomain: "versionfinalprog3.firebaseapp.com",
+  projectId: "versionfinalprog3",
+  storageBucket: "versionfinalprog3.appspot.com",
+  messagingSenderId: "129968517701",
+  appId: "1:129968517701:web:119055f3f4b7a9ca5da92c"
 };
 
 // Initialize Firebase
