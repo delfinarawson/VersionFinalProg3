ba89b3732bfde1839b335a10a777fcedfc346553
delfinarawson <drawson@udesa.edu.ar>
2023-11-11 19:12:55 -0300

    perfil

--- src/screens/MiPerfil/MiPerfil.js
+++ src/screens/MiPerfil/MiPerfil.js
@@ -1,55 +1,139 @@
 import React, { Component } from 'react';
-import {TextInput, TouchableOpacity, View, Text, StyleSheet, Image} from 'react-native';
-import { db, auth } from '../../firebase/config';
-import firebase from 'firebase';
+import {db, auth } from '../../firebase/config';
+import Post from '../../components/Post/Post';
+import {Image, TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList} from 'react-native';
 
 class MiPerfil extends Component {
     constructor(){
         super()
         this.state={
-            email: '',
-            nombre:'' ,
-            biografia: '',
-            foto: '',
-            cantposteo: '', 
-            borrar: false 
+            users: [],
+            listaPost: []
+        }   
+    }
+    componentDidMount(){
+        db.collection('users').where('owner', '==', auth.currentUser.email).onSnapshot(
+            docs =>{
+                let users = [];
+                docs.forEach( doc => {
+                    users.push({
+                       id: doc.id,
+                       data: doc.data()
+                    })
+                this.setState({
+                    users: users
+                })
+                })
+            }
+        )
+        db.collection('posts').where('owner', '==', auth.currentUser.email).onSnapshot(
+            posteos => {
+                let postsAMostrar = [];
 
-        }
+                posteos.forEach( unPost => {
+                    postsAMostrar.push(
+                        {
+                            id: unPost.id,
+                            datos: unPost.data()
+                        }
+                    )
+                })
+
+                this.setState({
+                    listaPost: postsAMostrar
+                })
+            }
+        )
     }
 
     logout(){
-        auth.signOut();
-
-        this.props.navigation.navigate('Login')
-
+        auth.signOut()
+        .then(() => {
+            this.props.navigation.navigate('Login')
+            console.log(auth.currentUser.email);
+        })
+        .catch(e => {console.log(e)})
     }
-    datosPerfil(){
-        db.collection('').onSnapshot 
-        // busco una collection con la info de mi usuario 
-    
-    }
-
 
     render(){
+        console.log(this.state);
         return(
             <View>
-                <Text>MI PERFIL</Text>
-                <TouchableOpacity onPressOut={()=>this.logout()}>
-                    <Text>Logout</Text>
+                <Text style={styles.screenTitle}>Profile</Text>
+                <View style={styles.mainContainer}>
+                <Text>{auth.currentUser.email}</Text>
+                <FlatList 
+                        data= {this.state.users}
+                        keyExtractor={ user => user.id }
+                        renderItem={ ({item}) => <Text>Username: {item.data.username}</Text> }
+                    />
+                <TouchableOpacity style={styles.button} onPress={()=>this.logout()}>
+                    <Text style={styles.textButton}>Log out</Text>
                 </TouchableOpacity>
-                <Text>NOMBRE DE USUARIO: </Text>
-                <Text>EMAIL:</Text>
-                <Text>BIOGRAFÍA:</Text>
-                <Image source={{url: ""}}/> 
-                <Text>CANTIDAD DE POSTEOS: </Text>
-                <TouchableOpacity onPressOut={()=>this.borrar()}>
-                    <Text>BORRAR POSTEO</Text>
-                </TouchableOpacity>
+                </View>
+                <Text style={styles.screenTitle}>My Posts</Text>
+                   
+                    <FlatList 
+                        data= {this.state.listaPost}
+                        keyExtractor={ unPost => unPost.id }
+                        renderItem={ ({item}) => <Post dataPost = { item } /> }
+                        style= {styles.listaPosts}
+                    />
+
+                
             </View>
-        )
+            
+        )}
+        }
+
+const styles = StyleSheet.create({
+    //CONTENEDOR GENERAL
+    screenTitle: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginLeft: 20,
+        marginVertical: 10
+    },
+    image: {
+        alignSelf: 'center',
+        height: 80,
+        width: "20%",
+        backgroundColor: 'white',
+        borderRadius: 10,
+        marginHorizontal: 100
+    },
+    mainContainer:{
+        flex: 1,
+        backgroundColor: '#ffffff',
+        borderRadius: 6,
+        marginHorizontal: 20,
+        padding: 5,
+        marginVertical: 5
+    },
+    button:{
+        alignSelf: 'flex-end',
+        height:30,
+        width: 150,
+        backgroundColor:'#46627f',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center',
+        borderRadius:4, 
+        borderWidth:1,
+        borderStyle: 'solid',
+        borderColor: '#46627f',
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textButton:{
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 15,
+        fontWeight: 'bold'
+
     }
-}
 
-
+})
 
 export default MiPerfil;
